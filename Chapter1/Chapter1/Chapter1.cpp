587e78f9f2bbfede7c11dccc6dfb9076308b6012
MrKangs <gykang00@gmail.com>
2019-08-13 00:37:14 -0700

    changed content in each programs

---
+++ Chapter1/Chapter1/Chapter1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+	//comments
+	std::cout << "Hello, world!\n";				// std:cout lives in the iostream library
+	std::cout << "It is nice to meet you!\n";	// these comments make the code hard to read
+	std::cout << "Yeah\n";						// especially when lines are different lengths
+												// therefore, use tab button to make them in line
+												/* or use these to comment a long paragraph
+												*/
+
+	int x;										// int x (same as java)
+	int a, b;									// int a; and new line int b;
+	double c; int d;							// double c; and new line int d;
+
+	int values = 4;								// copy initialization of value 4 into variable values
+	int yvalue(5);								// direct initialization of value 5 into variable yvalue
+	int width{ 5 };								// brace (uniform) initialization of value 5 into variable width (can hold any type)
+	int length{};								// zero initialization to value 0
+	int ab, bb(5);								// ab is define as int and bb is define as int and value is 5
+	int cb, db{ 5 };							// same idea from the preivous statement but just using brace
+												/* We can use this for direct initialization, but can't use for copy initialization*/
+	return 0;									
+												/* When an executable program finishes running, the program sends a value back to the operating 
+												system in order to indicate whether it ran successfully or not. This particular return statement
+												returns the value of 0 to the operating system, which means ¡°everything went okay!¡±. 
+												This is the last statement in the program that executes. */
+}
+
+/* must have a main function no matter what in any C++ programs*/
